9d9ebae55b16db003a7a1eadc4cbb95dbe225d68
Сейко Нікіта <2007nkt2007@gmail.com>
2025-10-29 22:52:14 +0200

    Update files after initial setup

---
+++ test_app.py
@@ -0,0 +1,9 @@
+from app import app
+
+def test_hello_route():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello from Docker" in response.data
+    student_surname_bytes = b"СЕЙКО"
+    assert student_surname_bytes in response.data
